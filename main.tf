99dc8126e05f50a7d5a8f6eec16f2478cd7cfbcb
Tiago Teixeira Santos <tiagots86@hotmail.com>
2024-11-04 14:04:12 -0300

    deployment e stage

--- main.tf
+++ main.tf
@@ -9,3 +9,26 @@ resource "aws_api_gateway_rest_api" "health_api" {
     types = ["REGIONAL"]
   }
 }
+
+resource "aws_api_gateway_deployment" "health_api" {
+  rest_api_id = aws_api_gateway_rest_api.health_api.id
+
+  triggers = {
+    redeploy = sha256(jsonencode(aws_api_gateway_rest_api.health_api.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "health_api" {
+  deployment_id = aws_api_gateway_deployment.health_api.id
+  rest_api_id   = aws_api_gateway_rest_api.health_api.id
+  stage_name    = var.environment
+
+  variables = {
+    vpcLinkId = data.aws_ssm_parameter.vpc_link.value
+  }
+
+}
